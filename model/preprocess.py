3076fbc52b5340b9f5aa08c984e5c362c8f8dcb0
pengzhengyi <zhengyi_peng@brown.edu>
2020-05-03 14:02:49 -0400

    complete preprocess script to re-parse corpora

--- model/preprocess.py
+++ model/preprocess.py
@@ -2,6 +2,8 @@ import argparse
 import pickle
 import re
 
+from tqdm import tqdm
+
 sentence_regex = re.compile(r'<(?P<speaker>.*)>\s*<(?P<sentiment>.*)>\s*(?P<sentence>.*)\s*')
 
 
@@ -51,7 +53,7 @@ def parse_sentences(sentences):
                 # skip first placeholder
                 yield {
                     'speaker': current_speaker,
-                    'sentence': ' '.join(current_sentences)
+                    'speech': ' '.join(current_sentences)
                 }
             current_sentences = []
             current_speaker = speaker
@@ -60,14 +62,55 @@ def parse_sentences(sentences):
 
     yield {
         'speaker': current_speaker,
-        'sentence': ' '.join(current_sentences)
+        'speech': ' '.join(current_sentences)
     }
 
 
-def parse_files(*filepaths):
+def pair_prompt_and_answer(source, answerer='Donald Trump'):
+    """
+    >>> sentences = ["<Sean Hannity> <Positive> All right. Joining us now live on the phone is, from the White House, President Donald Trump. Mr. President, I know you've been busy. I'm sure you never predicted this as part of any presidency. Thank you for spending time with us.", "<Donald Trump> <Positive> Thank you very much, Sean.", "<Sean Hannity> <Negative> Let me stay on this issue of hydroxychloroquine. And, you know, I'm going to quote this board-certified rheumatologist for Cedars-Sinai, Dr. Wallace, who I think, obviously, he's been prescribing this now, as he pointed out, 42 years in practice. He's been the head, as one of the largest lupus practices in the U.S., currently caring for 2,000 people, most taking HCQ; 400 peer-reviewed papers; chairman of the Lupus Foundation of America, the Rheumatoid Research Foundation of America College and -- and so many other credentials.", "<Sean Hannity> <Negative> And he said, In 42 years, no patient of mine has ever been hospitalized for HCQ. And he said the risk of taking it in the doses they're talking about -- in terms of a risk, he said it is nil -- absolutely nil, and you have been getting hammered for saying: what have you got to lose? Even the AMA is saying, well, your life.", "<Sean Hannity> <Positive> I don't know. He seems pretty credible to me, sir.", "<Donald Trump> <Positive> Well, it's been taken for malaria for many years and -- very effective. It's a powerful medicine; it's a powerful drug. But it's a drug that -- for malaria, for lupus, for those two things in particular. I guess some people say arthritis, too. But it's been taken for years and people are OK with it. It seems to be, with the azithromycin -- that really seems to be the combination that's great.", "<Donald Trump> <Positive> But that could cause a little problem -- people don't know, but it might cause a problem with the heart, in which case you don't take the azithromycin. That's for infection. But the combination -- and some people add zinc. But the combination has been pretty amazing. You saw the woman state representative, a Democrat -- state representative from Michigan, Detroit.", "<Donald Trump> <Positive> And she thought she was going to die, and she saw what we were talking about and she asked her husband to get it. And she would have never known about it. And he got it and she got better. She thought she had no chance, and she got better. She -- she's been very nice about it, actually. She -- I think she may be -- might be a Democrat, but she'll vote for me, maybe.", "<Donald Trump> <Negative> But she was very nice about it. So, you know, things are happening. It's a -- it's -- I haven't seen bad. I've not seen bad. And one thing that we do see is that people are not going to die from it. So if somebody is in trouble, you take it, I think."]
+    >>> source = parse_sentences(sentences)
+    >>> pairs = list(pair_prompt_and_answer(source))
+    >>> len(pairs)
+    2
+    """
+    prompt = None
+    prompter = None
+    for section in source:
+        speaker = section['speaker']
+        speech = section['speech']
+
+        if speaker != answerer:
+            # we want someone else asking answerer
+            # only tend to most recent prompt
+            prompt = speech
+            prompter = speaker
+        else:
+            if prompter is None:
+                # skip speech from answerer without previous prompt
+                continue
+            yield {
+                'prompter': prompter,
+                'prompt': prompt,
+                'answerer': answerer,
+                'answer': speech
+            }
+            prompt, prompter = None, None
+
+    # ignore last unanswered prompt (if exists)
+
+
+def parse_files(*filepaths, pipeline=[parse_sentences,
+                                      pair_prompt_and_answer]):
     for filepath in filepaths:
-        with open(filepath) as file_reader:
-            yield from parse_sentences(file_reader)
+        print(f'[i] Parsing {filepath}')
+        with tqdm(open(filepath)) as file_reader:
+
+            result = file_reader
+            for method in pipeline:
+                result = method(result)
+
+            yield from result
 
 
 def freeze_dataset(*filepaths, target=None):
