ab080572cab50464bd4ccc553a342f5e92101dbe
pengzhengyi <zhengyi_peng@brown.edu>
2020-05-04 00:03:39 -0400

    implement testing loop

--- model/model.py
+++ model/model.py
@@ -15,7 +15,7 @@ from torch.utils.data import DataLoader
 
 hyperparams = {
     "num_epochs": 3,
-    "batch_size": 4,
+    "batch_size": 16,
     "learning_rate": 6.25e-5
 }
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -45,8 +45,19 @@ class Model(nn.Module):
         self.device = device
         self.to(device)
 
-    def forward(self, input_ids, **kwargs):
-        return self.model(input_ids, **kwargs)
+    def forward(self, input_ids, mc_labels, **kwargs):
+        lm_loss, lm_logits, mc_logits, _ = self.model(input_ids, **kwargs)
+        mc_loss, loss = self.loss(lm_loss, mc_logits, mc_labels)
+        return (loss, lm_loss, mc_loss, lm_logits, mc_logits)
+
+    def __unpack_data(self, data):
+        return (
+            data['input_ids'].to(self.device),
+            data['attention_mask'].to(self.device),
+            data['token_type_ids'].to(self.device),
+            data['lm_labels'].to(self.device),
+            data['mc_labels'].to(self.device)
+        )
 
     def __compute_loss(self, lm_loss, mc_loss):
         """
@@ -118,50 +129,55 @@ class Model(nn.Module):
         print(f'loading model from {filepath}')
         self.load_state_dict(torch.load(filepath))
 
+    def accuracy(self, lm_logits, lm_labels):
+        preds = torch.argmax(lm_logits, dim=2)
+        num_valid_preds = torch.sum(lm_labels != -100).double()
+        num_correct_preds = torch.sum((preds == lm_labels))
+        accuracy = (num_correct_preds / num_valid_preds)
+        return (accuracy, num_correct_preds, num_valid_preds)
+
     def train_(self,
                train_dataloader: DataLoader,
                optimizer,
                experiment,
                num_epochs):
         self.train()
+
         total_lm_loss = torch.tensor(0.0)
         num_batches = 0
+
         with experiment.train():
             for which_epoch in range(num_epochs):
                 for train_data in tqdm(train_dataloader):
-                    optimizer.zero_grad()
                     num_batches += 1
+                    optimizer.zero_grad()
 
-                    input_ids = train_data['input_ids'].to(self.device)
-                    attention_mask = train_data['attention_mask'].to(
-                        self.device)
-                    token_type_ids = train_data['token_type_ids'].to(
-                        self.device)
-                    lm_labels = train_data['lm_labels'].to(self.device)
-                    mc_labels = train_data['mc_labels'].to(self.device)
+                    input_ids, attention_mask, token_type_ids, \
+                        lm_labels, mc_labels = self.__unpack_data(train_data)
 
-                    lm_loss, lm_logits, mc_logits, _ = self(
+                    loss, lm_loss, mc_loss, lm_logits = self(
                         input_ids,
+                        mc_labels,
                         attention_mask=attention_mask,
                         token_type_ids=token_type_ids,
-                        lm_labels=lm_labels)
+                        lm_labels=lm_labels
+                    )
 
-                    preds = torch.argmax(lm_logits, dim=2)
-                    num_valid_preds = torch.sum(lm_labels != -100).double()
-                    num_correct_preds = torch.sum((preds == lm_labels))
-                    accuracy = (num_correct_preds / num_valid_preds).item()
+                    accuracy, num_correct_preds, _ = self.accuracy(
+                        lm_logits, lm_labels)
 
-                    mc_loss, loss = self.loss(lm_loss, mc_logits, mc_labels)
                     loss.backward()
                     optimizer.step()
 
                     total_lm_loss += lm_loss
 
                     # log metrics
-                    experiment.log_metric('lm_loss', lm_loss)
-                    experiment.log_metric('mc_loss', mc_loss)
-                    experiment.log_metric('loss', loss)
-                    experiment.log_metric('accuracy', accuracy)
+                    experiment.log_metric('lm_loss', lm_loss.item())
+                    experiment.log_metric('mc_loss', mc_loss.item())
+                    experiment.log_metric('loss', loss.item())
+                    experiment.log_metric('correct_pedictions',
+                                          num_correct_preds.item())
+                    experiment.log_metric('accuracy', accuracy.item())
                 self.save(f'model-train-epoch{which_epoch}.pt')
 
         avg_word_loss = total_lm_loss / num_batches
@@ -170,6 +186,48 @@ class Model(nn.Module):
 
         tstr = ''.join(c if c.isdigit() else '-' for c in str(datetime.now()))
         self.save(f'model-train_{tstr}.pt')
+
+    def test_(self, test_dataloader, experiment):
+        self.eval()
+        total_lm_loss = torch.tensor(0.0)
+        total_correct_preds = torch.tensor(0.0)
+        total_valid_preds = torch.tensor(0.0)
+        with experiment.test():
+            with torch.no_grad():
+                for num_batches, test_data in tqdm(
+                        enumerate(test_dataloader, start=1)):
+                    input_ids, attention_mask, token_type_ids, \
+                        lm_labels, mc_labels = self.__unpack_data(test_data)
+
+                    loss, lm_loss, mc_loss, lm_logits = self(
+                        input_ids,
+                        mc_labels,
+                        attention_mask=attention_mask,
+                        token_type_ids=token_type_ids,
+                        lm_labels=lm_labels
+                    )
+
+                    accuracy, num_correct_preds, num_valid_preds = self.accuracy(
+                        lm_logits, lm_labels)
+
+                    total_lm_loss += lm_loss
+                    total_correct_preds += num_correct_preds
+                    total_valid_preds += num_valid_preds
+
+                    # log metrics
+                    experiment.log_metric('lm_loss', lm_loss.item())
+                    experiment.log_metric('mc_loss', mc_loss.item())
+                    experiment.log_metric('loss', loss.item())
+                    experiment.log_metric('correct_pedictions',
+                                          num_correct_preds.item())
+                    experiment.log_metric('accuracy', accuracy.item())
+
+        avg_word_loss = total_lm_loss / num_batches
+        perplexity = torch.exp(avg_word_loss)
+        accuracy = total_correct_preds / total_valid_preds
+
+        experiment.log_metric("final_perplexity", perplexity.item())
+        experiment.log_metric("final_accuracy", accuracy.item())
 
 
 if __name__ == '__main__':
@@ -182,6 +240,10 @@ if __name__ == '__main__':
         help='specify the model name to be loaded from ./models directory')
     parser.add_argument('-T', '--train', action='store_true',
                         help='run training loop')
+    parser.add_argument('-V', '--validate', action='store_true',
+                        help='run validation loop')
+    parser.add_argument('-t', '--test', action='store_true',
+                        help='run testing loop')
     args = parser.parse_args()
 
     # Make sure you modify the `.comet.config` file
@@ -204,3 +266,7 @@ if __name__ == '__main__':
             model.parameters(),
             lr=hyperparams['learning_rate'])
         model.train_(train_dataloader, optimizer, experiment, num_epochs)
+    if args.validate:
+        model.test_(validate_dataloader, experiment)
+    if args.test:
+        model.test_(test_dataloader, experiment)
